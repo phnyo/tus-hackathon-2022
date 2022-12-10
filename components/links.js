31313ad8a21270659eb5a477d2f6922f3760d80a
phnyo <sabanium@gmail.com>
2022-12-10 10:04:46 +0900

    Dev (#9)
    * dev (#8)
    
    * added links and router (#4)
    
    * Dev (#7)
    
    * added links and router
    
    * tailwind init
    
    * till 05:41
    
    * 10:04

--- components/links.js
+++ components/links.js
@@ -19,7 +19,7 @@ export default function LinkInputComponent() {
     <div>
       <div class="text-xl py-8 text-center align-middle">
         <form class="" onSubmit={handleSubmit} method="post">
-          <textarea type="text" id="text" name="text" rows="8" cols="20" class="w-11/12 place-center border-2 whitespace-pre-wrap"placeholder="文章を入力してください" />
+          <textarea type="text" id="text" name="text" rows="6" cols="20" class="w-11/12 place-center border-2 whitespace-pre-wrap"placeholder="文章を入力してください" />
           <button class="py-5" type="submit">ハイライトする</button>
         </form>
       </div>
--- pages/text.js
+++ pages/text.js
@@ -6,18 +6,18 @@ import Header from '../components/header';
 
 export default function Text(props) {
   
-  const router = useRouter()
-  
   return (
     <div class="text-xl py-8 xl:px-56 lg:px-40 md:px-32 sm:px-4 flex">
-      <div class="w-1/5">
+      <div class="md:w-1/5 sm:w-1/6">
       </div>
-      <div class="w-3/5 text-xl">
+      <div class="md:w-3/5 sm:w-4/6 text-xl">
         <Header />
-        <TextComponent text={JSON.stringify(props)}/>
-        <Link href="/"> 戻る </Link>
+        <TextComponent text={props}/>
+        <div class="center py-5 flex">
+          <Link href="/"> 戻る </Link>
+        </div>
       </div>
-      <div class="w-1/5">
+      <div class="md:w-1/5 sm:w-1/6">
       </div>
     </div>
   );
@@ -25,8 +25,50 @@ export default function Text(props) {
 
 export async function getServerSideProps(context) {
 
-  const res = highlightFetcher(context.query.text)
+  const res = await highlightFetcher(context.query.text)
+  
+  let words = []
+
+  if (res['entities'] != undefined) {
+    for (let i = 0; i < 8; i++) {
+      const name = res['entities'][i]['name'];
+      let prev_ind = -1;
+      if (res['entities'][i]['mentions'].length !== 0) {
+        for (let j = 0; j < res['entities'][i]['mentions'].length + 1; j++) {
+          const ind = context.query.text.indexOf(name, prev_ind + 1);
+          if (ind >= 0) {
+            words.push([name, ind]);
+          }
+          prev_ind = ind;
+        }
+      } else {
+        const ind = context.query.text.indexOf(name, prev_ind + 1);
+        words.push([name, ind]);
+      }
+    }
+  }
+  
+  words.sort((a, b) => a[1] -  b[1]);
+
+  let text = []
+  let ind = 0;
+  
+  for (let i = 0; i < words.length; i++) {
+    const front = words[i][1]; 
+    text.push(context.query.text.substring(ind, front));
+    text.push(words[i][0]);
+    ind = front + words[i][0].length;
+  }
+  if (context.query.text != undefined) {
+    text.push(context.query.text.substring(ind, context.query.text.length))
+  }
+
+  console.log(words)
+
   return {
-    props: res
+    props: {
+      text: text,
+      words: words,
+    }
   }
 }
