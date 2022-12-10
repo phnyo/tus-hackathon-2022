b453e6671f18784c084ae85611876e4fd07aa4d5
nyaos <sabanium@gmail.com>
2022-12-10 11:43:48 +0900

    res confilict

--- components/text.js
+++ components/text.js
@@ -1,6 +1,7 @@
 import styles from './mark.module.css'
 
 export default function TextComponent({text}) {
+<<<<<<< HEAD
   if (!text || text.length === 0 || text === []) {
     return <div><p> 何も入力されていません。</p></div>
   }
@@ -10,6 +11,17 @@ export default function TextComponent({text}) {
       return <mark className={styles.mark} key={index}> {value} </mark>
     } else {
       return <span key={index}> {value} </span>
+=======
+  if (text.length === 0 || text === []){
+    return <div><p> 何も入力されていません。</p></div>
+  }
+
+  const item = text['text'].map(function(value, index) {
+    if (index % 2 === 1) {
+      return <mark> {value} </mark>
+    } else {
+      return <span> {value} </span>
+>>>>>>> e155a4c (10:04)
     }
   })
 
